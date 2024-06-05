6d8d5e4d689972e04deb3bd70bc763c3234dbcb8
Hanntuu <raihanrrdarmawan@gmail.com>
2024-06-05 22:28:57 +0700

    Setup Project

---
+++ NutriSeeApp/app/src/main/java/com/dicoding/nutriseeapp/main/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.nutriseeapp.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.nutriseeapp.R
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
