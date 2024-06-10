736ca3c0d2c503e568993fc530a9c43a92f17aee
dzakiys2 <C296D4KY0418@bangkit.academy>
2024-06-10 07:35:06 +0700

    feat: API endpoint GET /products to get all products data

--- main-api/src/server.js
+++ main-api/src/server.js
@@ -4,6 +4,7 @@ const routes = require('./routes');
 const admin = require('firebase-admin');
 const multer = require('multer');
 const upload = multer();
+const { nutriScoreCalculator } = require('./functions/nutriscoreCalculator.js');
 
 app.use(express.json());
 const databaseURL = process.env.DATABASE_URL;
@@ -14,28 +15,36 @@ admin.initializeApp({
 
 app.use('/', routes);
 
-app.post('/get-nutrition', upload.none(), async (req, res) => {
-  try {
-      const { productName } = req.body;
-      console.log('Request Body:', req.body);
-      if (!productName) {
-          return res.status(400).json({ error: 'Product name is required' });
-      }
+app.get('/products', async (req, res) => {
+    try {
+        const dbRef = admin.database().ref('products');
+        const snapshot = await dbRef.once('value');
+        const products = snapshot.val();
+  
+        if (!products) {
+            return res.status(404).json({ error: 'No products found' });
+        }
+        
+        // Convert products object into an array of objects
+        const productsArray = Object.keys(products).map(key => ({
+            id: key,
+            ...products[key]
+        }));
+        
+        // Organize products into an object with numeric keys
+        const productsObject = {};
+        productsArray.forEach((product, index) => {
+            productsObject[index] = product;
+        });
 
-      const dbRef = admin.database().ref('products').child(productName);
-      const snapshot = await dbRef.once('value');
-      const nutritionData = snapshot.val();
-
-      if (!nutritionData) {
-          return res.status(404).json({ error: 'Product not found' });
-      }
-
-      res.status(200).json({ productName, size: "100", nutrition: nutritionData });
-  } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
-  }
+        res.status(200).json(productsObject);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
+
+nutriScoreCalculator();
 
 const port = process.env.PORT || 6000;
 app.listen(port, () => {
