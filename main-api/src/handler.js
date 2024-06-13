d264ef61befb8137add48323b8d6e8a76e60cd75
dzakiys2 <C296D4KY0418@bangkit.academy>
2024-06-13 13:40:39 +0700

    feat: GET /history endpoint

--- main-api/src/handler.js
+++ main-api/src/handler.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const filterEmail = require('./helper/emailFilter');
 const serviceAccount = process.env.SERVICE_ACCOUNT_KEY;
 const projectId = process.env.PROJECT_ID;
+const { v4: uuidv4 } = require('uuid');
 
 const admin = require('firebase-admin');
 
@@ -17,7 +18,7 @@ const storage = new Storage({
 const bucketName = process.env.BUCKET_NAME;
 
 const mlAPI = process.env.ML_API_URL;
-
+console.log(mlAPI);
 const uploadImage = async (req, res) => {
   try {
     console.log('Request Headers:', req.headers);
@@ -57,7 +58,17 @@ const uploadImage = async (req, res) => {
             return res.status(404).json({ error: 'Product not found' });
         }
     
+        let statusLogoUrl = null;
+        if (productData.isHalal) {
+          statusLogoUrl = `https://storage.googleapis.com/${bucketName}/halal-logo.png`; // Adjust the path as necessary
+        } 
+        else {
+          statusLogoUrl = `https://storage.googleapis.com/${bucketName}/haram-logo.png`;
+        }
+
         const data = {
+          history_id: uuidv4(),
+          product_barcode: uuidv4(),
           merek: productData.merek,
           nama_produk: productData.nama_produk,
           ukuran_porsi: productData.ukuran_porsi,
@@ -83,14 +94,17 @@ const uploadImage = async (req, res) => {
           type: productData.type,
           sub_type: productData.sub_type,
           confidence: confidenceResult,
+          isHalal: productData.isHalal, // Indicate if the product is halal
+          status_logo_url: statusLogoUrl, // URL of the halal or haram logo
+          timestamp: admin.database.ServerValue.TIMESTAMP 
         };
     
+        res.status(200).json({data});
         // Save history data to Firebase
         const historyRef = admin.database().ref('history').child(filteredEmail).push();
         await historyRef.set(data);
-
-        res.status(200).json({data});
-  } catch (error) {
+  
+      } catch (error) {
         console.error(error);
         res.status(500).json({error: 'Internal server error'});
   }
@@ -158,6 +172,64 @@ const getToken = async (req, res) => {
   }
 };
 
+const getHistoryById = async (req, res) => {
+  const historyId = req.params.historyId;
+  const email = "atmin.nutrisee@gmail.com"; //req.user.email
+  const filteredEmail = filterEmail(email);
 
+  try {
+    const historyRef = admin.database().ref('history').child(filteredEmail).child(historyId);
+    const snapshot = await historyRef.once('value');
+    const historyData = snapshot.val();
 
-module.exports = { uploadImage, getToken };
+    if (!historyData) {
+      return res.status(404).json({ error: 'History not found' });
+    }
+
+    res.status(200).json({ data: historyData });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const getHistoryByUser = async (req, res) => {
+  const email = "atmin.nutrisee@gmail.com"; //req.user.email
+  const filteredEmail = filterEmail(email);
+
+  try {
+    const historyRef = admin.database().ref('history').child(filteredEmail).orderByChild('timestamp');
+    const snapshot = await historyRef.once('value');
+    const historyData = [];
+
+    snapshot.forEach(childSnapshot => {
+      historyData.push({ id: childSnapshot.key, ...childSnapshot.val() });
+    });
+
+    res.status(200).json({ data: historyData.reverse() }); // Reverse to get newest first
+  } catch (error) {
+    console.error('Error retrieving history:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const getProductById = async (req, res) => {
+  const productId = req.params.productId;
+
+  try {
+    const productRef = admin.database().ref('products').child(productId);
+    const snapshot = await productRef.once('value');
+    const productData = snapshot.val();
+
+    if (!productData) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    res.status(200).json({ data: productData });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+module.exports = { uploadImage, getToken, getHistoryById, getProductById, getHistoryByUser };
--- main-api/src/routes.js
+++ main-api/src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { uploadImage, getToken } = require('./handler');
+const { uploadImage, getToken, getHistoryById, getProductById, getHistoryByUser } = require('./handler');
 const upload = multer();
 const {verifyToken} = require('./middleware');
 
@@ -73,4 +73,168 @@ router.post('/upload', verifyToken, upload.single('image'), uploadImage);
 
 router.post('/get-token', getToken);
 
+
+
+/**
+ * @swagger
+ * /history/{historyId}:
+ *   get:
+ *     summary: Get history by ID
+ *     description: Retrieve specific history details by history ID.
+ *     tags:
+ *       - History
+ *     parameters:
+ *       - in: path
+ *         name: historyId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the history entry
+ *     responses:
+ *       200:
+ *         description: History details retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       404:
+ *         description: History not found.
+ */
+router.get('/history/:historyId', getHistoryById, verifyToken);
+  
+  /**
+   * @swagger
+   * /products/{productId}:
+   *   get:
+   *     summary: Get product by ID
+   *     description: Retrieve specific product details by product ID.
+   *     tags:
+   *       - Products
+   *     parameters:
+   *       - in: path
+   *         name: productId
+   *         schema:
+   *           type: string
+   *         required: true
+   *         description: The ID of the product entry
+   *     responses:
+   *       200:
+   *         description: Product details retrieved successfully.
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *       404:
+   *         description: Product not found.
+   */
+  router.get('/products/:productId', getProductById, verifyToken);
+
+  /**
+ * @swagger
+ * /history:
+ *   get:
+ *     summary: Get user history
+ *     description: Retrieve the user's history sorted by the newest entries.
+ *     tags:
+ *       - History
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved user history.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                         description: Unique identifier for the history entry.
+ *                       brand:
+ *                         type: string
+ *                         description: Brand of the product.
+ *                       product_name:
+ *                         type: string
+ *                         description: Name of the product.
+ *                       serving_size:
+ *                         type: string
+ *                         description: Serving size of the product.
+ *                       nutriscore:
+ *                         type: string
+ *                         description: Nutri-Score of the product.
+ *                       nutrition:
+ *                         type: object
+ *                         properties:
+ *                           energi:
+ *                             type: number
+ *                             description: Energy content of the product.
+ *                           karbohidrat:
+ *                             type: object
+ *                             properties:
+ *                               gula:
+ *                                 type: number
+ *                                 description: Sugar content of the product.
+ *                               total:
+ *                                 type: number
+ *                                 description: Total carbohydrate content of the product.
+ *                           lemak:
+ *                             type: object
+ *                             properties:
+ *                               jenuh:
+ *                                 type: number
+ *                                 description: Saturated fat content of the product.
+ *                               total:
+ *                                 type: number
+ *                                 description: Total fat content of the product.
+ *                               trans:
+ *                                 type: number
+ *                                 description: Trans fat content of the product.
+ *                           protein:
+ *                             type: number
+ *                             description: Protein content of the product.
+ *                           sodium:
+ *                             type: number
+ *                             description: Sodium content of the product.
+ *                       nutrition_fact_image:
+ *                         type: string
+ *                         description: URL to the nutrition fact image.
+ *                       product_image:
+ *                         type: string
+ *                         description: URL to the product image.
+ *                       summary:
+ *                         type: string
+ *                         description: Summary of the product.
+ *                       servings:
+ *                         type: number
+ *                         description: Number of servings.
+ *                       type:
+ *                         type: string
+ *                         description: Type of the product.
+ *                       subtype:
+ *                         type: string
+ *                         description: Subtype of the product.
+ *                       confidence:
+ *                         type: number
+ *                         description: Confidence score of the product classification.
+ *                       halal_status:
+ *                         type: boolean
+ *                         description: Halal status of the product.
+ *                       status_logo_url:
+ *                         type: string
+ *                         description: URL of the status logo (halal or haram).
+ *                       timestamp:
+ *                         type: integer
+ *                         format: int64
+ *                         description: Timestamp of the history entry.
+ *       401:
+ *         description: Unauthorized - invalid or missing token.
+ *       500:
+ *         description: Internal server error.
+ */
+  router.get('/history', getHistoryByUser, verifyToken);
 module.exports = router;
